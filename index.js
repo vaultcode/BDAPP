f593efa58fce77b830ec96cf11eabfe723710168
rajeshpanchaneni <rajesh_panchaneni@vaultcode.org>
2018-07-11 16:37:56 +0530

    Header creation

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+//import a library yo help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/Header';
 
+//create a component
+const App = () => (
+     <Header headerText={'BloodGroup'} />
+  );
+
+//render it to the device
 AppRegistry.registerComponent('bloodapp', () => App);
