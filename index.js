292a46759230c9e61657c0315210230199682ec7
rajeshpanchaneni <rajesh_panchaneni@vaultcode.org>
2018-07-17 18:51:17 +0530

    Creating Second Button

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import Button from './src/components/Button';
 
+
 //create a component
 const App = () => (
   <View style={{ flex: 1 }}>
      <Header headerText={'BloodApp'} />
      <Button />
+
   </View>
   );
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 
 const Button = ({ onPress }) => {
   const { buttonStyle, textStyle } = styles;
    return (
+     <View>
      <TouchableOpacity onPress={onPress} style={buttonStyle}>
         <Text style={textStyle}>Donate
         </Text>
     </TouchableOpacity>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+       <Text style={textStyle}>Request
+       </Text>
+   </TouchableOpacity>
+   </View>
   );
 };
 
